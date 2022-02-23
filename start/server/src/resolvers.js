81a5ee2932fc9f0b65b36bb76bd45ff9f8cd7458
Jihyun <bjih1999@naver.com>
2022-02-23 16:33:04 +0900

    fix: syntax error

--- start/server/src/resolvers.js
+++ start/server/src/resolvers.js
@@ -1,8 +1,8 @@
-const { paginateResults } = require('./utils');
+const { paginateResults } = require("./utils");
 
 /**
  *  resolver
- * 
+ *
  *  - graphql에 정의된 속성들 수행하기 위한 동작이 정의되어 있음
  *      - MVC 패턴의 Controller와 유사하게 사용될 수 있음
  *      - 데잍를 생서하는 로직이 resolver에 포함되어도 되지만, service 레이어로 한번 분리하여 호출하는 것을 추천
@@ -14,60 +14,96 @@ const { paginateResults } = require('./utils');
  *      - info : 현재 동작에 대한 상태 정보를 답고 있다(고 한다. 정확하게는 모르겠다.)
  */
 module.exports = {
-    Query: {
-      launches: async (_, { pageSize = 20, after }, { dataSources }) => {
-        const allLaunches = await dataSources.launchAPI.getAllLaunches();
-        allLaunches.reverse();
+  Query: {
+    launches: async (_, { pageSize = 20, after }, { dataSources }) => {
+      const allLaunches = await dataSources.launchAPI.getAllLaunches();
+      allLaunches.reverse();
 
-        const launches = paginateResults({
-            after,
-            pageSize,
-            results: allLaunches
-        });
+      const launches = paginateResults({
+        after,
+        pageSize,
+        results: allLaunches,
+      });
 
+      return {
+        launches,
+        cursor: launches.lenght ? launches[launches.length - 1].cursor : null,
+
+        hasMore: launches.length
+          ? launches[launches.length - 1].cursor !==
+            allLaunches[allLaunches.length - 1].cursor
+          : false,
+      };
+    },
+    launch: (_, { id }, { dataSources }) =>
+      dataSources.launchAPI.getLaunchById({ launchId: id }),
+    me: (_, __, { dataSources }) => dataSources.userAPI.findOrCreateUser(),
+  },
+
+  Mutation: {
+    login: async (_, { email }, { dataSources }) => {
+      const user = await dataSources.userAPI.findOrCreateUser({ email });
+      if (user) {
+        user.token = Buffer.from(email).toString("base64");
+        return user;
+      }
+    },
+
+    bookTrips: async (_, { launchIds }, { dataSources }) => {
+      const results = await dataSources.userAPI.bookTrips({ launchIds });
+      const launches = await dataSources.launchAPI.getLaunchesByIds({
+        launchIds,
+      });
+
+      return {
+        success: results && results.length === launchIds.length,
+        message:
+          results.length === launches.length
+            ? "trips booked successfully"
+            : `the following launches couldnt be booked: ${launchIds.filter(
+                (id) => !results.includes(id)
+              )}`,
+      };
+    },
+
+    cancelTrip: async (_, { launchId }, { dataSources }) => {
+        const result = await dataSources.userAPI.cancelTrip({ launchId });
+    
+        if (!result) {
+          return {
+            success: false,
+            message: "failed to cancel trip",
+          };
+        }
+    
+        const launch = await dataSources.launchAPI.getLaunchById({ launchId });
+    
         return {
-            launches,
-            cursor: launches.lenght ? launches[launches.length - 1].cursor : null,
-
-            hasMore: launches.length
-                ? launches[launches.length - 1].cursor !== allLaunches[allLaunches.length - 1].cursor
-                : false
+          success: true,
+          message: "trip cancelled",
+          launches: [launch],
         };
       },
-      launch: (_, { id }, { dataSources }) =>
-        dataSources.launchAPI.getLaunchById({ launchId: id }),
-      me: (_, __, { dataSources }) => dataSources.userAPI.findOrCreateUser()
-    },
+  },
 
-    Mutation: {
-        login: async (_, { email }, { dataSources }) => {
-            const user = await dataSources.userAPI.findOrCreateUser({ email });
-            if (user) {
-                user.token = Buffer.from(email).toString('base64');
-                return user;
-            }
-        }
+  Mission: {
+    missionPatch: (mission, { size } = { size: "LARGE" }) => {
+      return size === "SMALL"
+        ? mission.missionPatchSmall
+        : mission.missionPatchLarge;
     },
+  },
 
-    Mission: {
-        missionPatch: (mission, { size } = { size: 'LARGE' }) => {
-            return size === 'SMALL'
-                ? mission.missionPatchSmall
-                : mission.missionPatchLarge;
-        },
+  Launch: {
+    isBooked: async (_, __, { dataSources }) => {
+      const launchIds = await dataSources.userApi.getLaunchIdsByUser();
+      if (!launchIds.length) return [];
+
+      return (
+        dataSources.launchAPI.getLaunchesByIds({
+          launchIds,
+        }) || []
+      );
     },
-
-    Launch: {
-        isBooked: async (_, __, { dataSources }) => {
-            const launchIds = await dataSources.userApi.getLaunchIdsByUser();
-            if (!launchIds.length) return [];
-
-            return (
-                dataSources.launchAPI.getLaunchesByIds({
-                    launchIds,
-                }) || []
-            );
-        },
-    },
-  };
-  
+  },
+};
